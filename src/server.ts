7ed5304b51f3f4d871f23b32acbb6e25a90f61d8
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2023-02-18 17:13:22 -0600

    server and demo

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 import path from 'path';
 
 const app = express()
@@ -23,4 +23,50 @@ app.get('/api', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+})*/
+
+
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express' 
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { typeDefs } from './schemas/schema';  
+import { resolvers } from './resolvers/resolvers';
+
+dotenv.config();
+
+async function startApolloServer() {
+    
+    const app = express();
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+
+    const PORT = process.env.PORT || 4000;
+    await new Promise<void>(resolve => app.listen({ port: PORT },resolve))
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    }
+
+    async function startMongoDB() {
+        try{
+            await mongoose.connect(process.env.MONGODB_URI!)
+            console.log('🟢 MongoDB connected!');
+        }catch(error){
+            console.error('🔴 MongoDB connection error:', error);
+            process.exit(1);
+        }
+    }
+
+    async function startServer(){
+        await startMongoDB();
+        await startApolloServer();
+    }
+
+    startServer();
+
+
