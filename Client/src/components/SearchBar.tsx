fea6b810655ecbc26ca8813a4c03bd52398c63c9
Denis <dsalazar260990@outlook.com>
2023-06-11 11:01:36 -0600

    Update SearchBar.tsx

--- Client/src/components/SearchBar.tsx
+++ Client/src/components/SearchBar.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { TextField, Stack } from "@mui/material";
+import { TextField, Stack, IconButton } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 const styles = {
   root: {
-    '& > *': {
-      width: '25ch',
-    },
+      width: '95%',
+      display : 'flex',
+      flexDirection:'row',
+      justifyContent:' center'
+  },
+  textfield : {
+    width : '30%',
+    ml : '12%'
   },
 };
 
 function SearchBar() {
   return (
     <Stack sx={styles.root} >
-      <TextField id="search-bar" label="Search" variant="outlined" />
+      <TextField id="search-bar" label="Search" variant="outlined" sx={styles.textfield} />
+      <IconButton>
+      <SearchIcon id='search-button' fontSize='large'/>
+      </IconButton>
     </Stack>
   );
 }
