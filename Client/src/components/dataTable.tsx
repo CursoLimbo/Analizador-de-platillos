d50fdf3ca6de61472aeb41abcee8b0379085070d
Denis Salazar <107601365+Dns2690@users.noreply.github.com>
2023-07-19 22:45:53 -0600

    table component and ingredients

--- Client/src/components/dataTable.tsx
+++ Client/src/components/dataTable.tsx
@@ -1,26 +1,28 @@
-import * as React from 'react';
+
+
+import { useState} from 'react';
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import AddIcon from '@mui/icons-material/Add';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 
+
 interface RowData {
   id: number;
   [key: string]: string | number | null;
 }
-
 interface DataGridProps {
+  tableName:string;
   rows: RowData[];
   columns: GridColDef[];
 }
 
-const DataGridInfo: React.FC<DataGridProps> = ({ rows, columns }) => {
-  const [selectedIds, setSelectedIds] = React.useState<number[]>([]);
-  const [selectedRow, setSelectedRow] = React.useState<RowData | null>(null);
-
+const DataGridInfo: React.FC<DataGridProps> = ({ rows, columns,tableName }) => {
+  const [selectedIds, setSelectedIds] = useState<number[]>([]);
+  const [selectedRow, setSelectedRow] = useState<RowData | null>(null);
   const handleSelectionChange = (selection: any[]) => {
     const selectedRowIds = selection.map((item) => item.id);
     setSelectedIds(selectedRowIds);
@@ -34,26 +36,26 @@ const DataGridInfo: React.FC<DataGridProps> = ({ rows, columns }) => {
   };
 
   const handleDeleteSelected = () => {
-    // Implement your logic to delete the selected rows based on the selectedIds array.
+
     console.log('Deleting selected rows:', selectedIds);
     setSelectedIds([]);
   };
 
   const handleEditSelected = () => {
-    // Implement your logic to edit the selected row.
+
     console.log('Editing selected row:', selectedRow);
   };
 
   const handleAddRow = () => {
-    // Implement your logic to add a new row.
+
     console.log('Adding a new row');
   };
 
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ height: '80vh', width: '100%' }}>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
         <Typography variant="h6" component="div">
-          My Data Table
+          {tableName}
         </Typography>
         <Box>
           {selectedIds.length > 0 && (
@@ -86,20 +88,6 @@ const DataGridInfo: React.FC<DataGridProps> = ({ rows, columns }) => {
   );
 };
 
-// Ejemplo de uso con datos y columnas dinámicos
-const data: RowData[] = [
-  { id: 1, firstName: 'John', lastName: 'Doe', age: 30 },
-  { id: 2, firstName: 'Jane', lastName: 'Smith', age: 25 },
-  { id: 3, firstName: 'Bob', lastName: 'Johnson', age: 40 },
-];
+export default DataGridInfo;
 
-const columns: GridColDef[] = Object.keys(data[0]).map((key) => ({
-  field: key,
-  headerName: key.charAt(0).toUpperCase() + key.slice(1),
-  width: 150,
-  editable: true,
-}));
 
-export default function App() {
-  return <DataGridInfo rows={data} columns={columns} />;
-}
