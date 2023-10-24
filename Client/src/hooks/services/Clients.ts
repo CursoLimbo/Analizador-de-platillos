2d3571a31c37bf71e3c2986ded5adf3093290ad5
loydgarmany <141684401+loydgarmany@users.noreply.github.com>
2023-10-23 19:55:29 -0600

    Update Clients.ts

--- Client/src/hooks/services/Clients.ts
+++ Client/src/hooks/services/Clients.ts
@@ -1,6 +1,22 @@
 import {useMutation, useQuery} from "@apollo/react-hooks";
-import {CreateClientMutation} from "services/Client-Service";
+import {DeleteClient, GetAllClients, UpdateClient, GetClientById, CreateClientMutation} from "services/Client-Service";
 
 export const useCreateClientMutation = () => {
     return useMutation(CreateClientMutation);
 } 
+
+export const useUpdateClientMutation = () => {
+    return useMutation(UpdateClient)
+} 
+
+export const useDeleteClientMutation = () => {
+    return useMutation(DeleteClient)
+} 
+
+export const useGetAllClients = () => {
+    return useQuery(GetAllClients)
+} 
+
+export const useGetClientById = (id:string) => {
+    return useQuery(GetClientById, {variables:{"getClientId":id}})
+}
