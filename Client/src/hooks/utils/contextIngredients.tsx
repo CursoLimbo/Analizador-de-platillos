6a814ba33031200ca89681e1b56239842df37dce
Denis Salazar <107601365+Dns2690@users.noreply.github.com>
2023-12-05 21:56:27 -0600

    Context and form CostRecipe

--- Client/src/hooks/utils/contextIngredients.tsx
+++ Client/src/hooks/utils/contextIngredients.tsx
@@ -1,49 +1,57 @@
-import React,{ createContext, useContext,useState, ReactNode} from "react";
-
-
+import React, { createContext, useContext, useState, ReactNode } from "react";
 
 // defines the state of the context
 
 interface IContext {
-    ingredientsIDsArray: RecipeIngredient[];
-    setIngredientsIDsArray: React.Dispatch<React.SetStateAction<RecipeIngredient[]>>;
-    Recipe : Recipe;
-    setRecipe: React.Dispatch<React.SetStateAction<Recipe>>;
-
-    
+  ingredientsIDsArray: RecipeIngredient[];
+  setIngredientsIDsArray: React.Dispatch<
+    React.SetStateAction<RecipeIngredient[]>
+  >;
+  Recipe: Recipe;
+  setRecipe: React.Dispatch<React.SetStateAction<Recipe>>;
+  costConf: CostConf;
+  setCostConf: React.Dispatch<React.SetStateAction<CostConf>>;
 }
 
-// defines the context  
+// defines the context
 
 const Context = createContext<IContext>({} as IContext);
 
 //create provider
 
 export const ContextProvider = ({ children }: { children: ReactNode }) => {
-    const [ingredientsIDsArray, setIngredientsIDsArray] = useState<RecipeIngredient[]>([]);
-    const [Recipe, setRecipe] = useState<Recipe>({
-        name: '',
-        portions :  0,
-        procedure: ''
-      });
+  const [ingredientsIDsArray, setIngredientsIDsArray] = useState<
+    RecipeIngredient[]
+  >([]);
+  const [Recipe, setRecipe] = useState<Recipe>({
+    name: "",
+    portions: 0,
+    procedure: "",
+  });
+  const [costConf, setCostConf] = useState<CostConf>({
+    inflation: 0,
+    IVA: 0,
+    ISA: 0,
+    utilities: 0,
+    profits: 0,
+  });
 
-
-    return (
-        <Context.Provider
-            value={{
-                ingredientsIDsArray,
-                setIngredientsIDsArray,
-                Recipe,
-                setRecipe,
-            }}
-        >
-            {children}
-        </Context.Provider>
-    );
-}
+  return (
+    <Context.Provider
+      value={{
+        ingredientsIDsArray,
+        setIngredientsIDsArray,
+        Recipe,
+        setRecipe,
+        costConf,
+        setCostConf
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
 
 // hook access to the context
 
 export const useContextData = () => useContext(Context);
-
-
--- Client/src/pages/costRecipeConf.tsx
+++ Client/src/pages/costRecipeConf.tsx
@@ -1,17 +1,106 @@
 import React from "react";
+import {
+  Stack,
+  TextField,
+} from "@mui/material";
+import { AppButton } from "../components/Button"; 
+import { ConfirmAlert } from "../components/sweetAlert"; 
+import { useContextData } from "../hooks/utils/contextIngredients";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 
+type CostData = {
+  inflation: number;
+  IVA: number;
+  ISA: number;
+  utilities: number;
+  profits: number;
+};
 
+const CostRecipeConf: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CostData>();
+  const router = useRouter();
+  const { costConf, setCostConf } = useContextData();
 
+  const onSubmit  = async (data: CostData) => {
+    const newCostConf: CostData = {
+      inflation: Number(data.inflation),
+      IVA: Number(data.IVA),
+      ISA: Number(data.ISA),
+      utilities: Number(data.utilities),
+      profits: Number(data.profits),
+    };
 
+    const confirm = await ConfirmAlert();
+    if (confirm) {
+      setCostConf(newCostConf);
+      router.back();
+    }
+  };
 
-const CostRecipeConf:React.FC = ()=>{
-
-
-
-    return (
-        <h1>Hola Mundo</h1>
-    )
-}
-
+  return (
+    <Stack alignItems="center" fontFamily="Times New Roman" fontSize={28}>
+      <h1>Costeo Receta</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack alignItems="center">
+          <Stack width={250} gap={5}>
+            <TextField
+              id="inflationNumber"
+              label="Inflación (%)"
+              variant="outlined"
+              type="number"
+              {...register("inflation", { required: true })}
+              error={!!errors.inflation}
+              helperText={errors.inflation && "Este campo es requerido"}
+            />
+            <TextField
+              id="IVANumber"
+              label="Impuesto de Venta (%)"
+              variant="outlined"
+              type="number"
+              {...register("IVA", { required: true })}
+              error={!!errors.IVA}
+              helperText={errors.IVA && "Este campo es requerido"}
+            />
+            <TextField
+              id="ISANumber"
+              label="Impuesto de Servicio (%)"
+              variant="outlined"
+              type="number"
+              {...register("ISA", { required: true })}
+              error={!!errors.ISA}
+              helperText={errors.ISA && "Este campo es requerido"}
+            />
+            <TextField
+              id="utilitiesNumber"
+              label="Utilidades (%)"
+              variant="outlined"
+              type="number"
+              {...register("utilities", { required: true })}
+              error={!!errors.utilities}
+              helperText={errors.utilities && "Este campo es requerido"}
+            />
+            <TextField
+              id="profitsNumber"
+              label="Ganancia (%)"
+              variant="outlined"
+              type="number"
+              {...register("profits", { required: true })}
+              error={!!errors.profits}
+              helperText={errors.profits && "Este campo es requerido"}
+            />
+          </Stack>
+          <Stack marginTop={5}>
+            <AppButton type="submit">GUARDAR</AppButton>
+          </Stack>
+        </Stack>
+      </form>
+    </Stack>
+  );
+};
 
 export default CostRecipeConf;
